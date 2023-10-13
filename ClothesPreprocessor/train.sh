73e95ffa229f746e48182421b42645d29ee8a5e7
ayushman <ayushman@dashtoon.com>
2023-10-13 04:24:49 -0700

    fix env and update train.sh

--- ClothesPreprocessor/train.sh
+++ ClothesPreprocessor/train.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 timestamp=$(date +%Y%m%d-%H%M%S)
-DETECTRON2_DATASETS="/mnt/disk1/repos/dashtoon-research/ClothesPreprocessor/datasets" python train_net.py \
+CUDA_VISIBLE_DEVICES=2,3 DETECTRON2_DATASETS="/mnt/data1/ayushman/datasets" python train_net.py \
         --config-file configs/DensePose-EFFV2-BiFPN-Keypoint-Seg.yaml \
+        --num-gpus 2 \
+        --num-machines 1 \
         OUTPUT_DIR ./output/${timestamp} \
         CUDNN_BENCHMARK True
